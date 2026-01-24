4d496b2eb3646b9a2cc542309888e14982ad3261
amir.espahbodi <dev.amir.espahbodi@gmail.com>
2026-01-24 13:52:08 +0330

    update docker ignore

--- app/api/main.py
+++ app/api/main.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, principles, samples, users
+from app.api.routes import health, login, principles, samples, users
 
 api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(principles.router)
 api_router.include_router(samples.router)
+api_router.include_router(health.router)
