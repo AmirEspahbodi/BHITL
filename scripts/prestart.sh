803888cf67c4898a6190e8e43f8c9301c6c7714b
amir.espahbodi <dev.amir.espahbodi@gmail.com>
2026-01-26 09:07:14 +0330

    changes

--- scripts/prestart.sh
+++ scripts/prestart.sh
@@ -3,8 +3,8 @@
 set -e
 set -x
 
-echo "Let the DB start"
-poetry run python -m app.backend_pre_start
+# echo "Let the DB start"
+# poetry run python -m app.backend_pre_start
 
 echo "Run migrations"
 poetry run alembic revision --autogenerate -m "New Migration"
